00d624d5a76e264f78fc260abddb48c30f6654b9
MajesticNthg <majestic_nthg@mail.ru>
2024-03-09 16:55:11 +1000

    mod.2

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+?
+src/Level1.java,0\7\07084fabeefc82b5120f1af8a8f1b29bd1b8c52a
+S
+#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
+Å
+Q.idea/sonarlint/securityhotspotstore/0/7/07084fabeefc82b5120f1af8a8f1b29bd1b8c52a,3\7\37ff92c265db56e65336b84efedd78846e98d30e
+]
+-.idea/sonarlint/securityhotspotstore/index.pb,8\8\88a9255124c95bdc913197c120a8d560edc59c8e
--- src/Level1.java
+++ src/Level1.java
@@ -20,32 +20,42 @@ public class Level1 {
 
         int day = 1;
         int flag = 1;
+        int amt = 0;
         boolean complete = false;
         while (!complete) {
             for (int i = 0; i < matrix.length; i++) {
                 for (int j = 0; j < matrix[i].length; j++) {
                     if (matrix[i][j] == flag) {
-                        if (i != (matrix.length - 1) && matrix[i + 1][j] == 0)
+                        if (i != (matrix.length - 1) && matrix[i + 1][j] == 0) {
                             matrix[i + 1][j] = flag + 1;
-                        if (i != 0 && matrix[i - 1][j] == 0)
+                            amt++;
+                        }
+                        if (i != 0 && matrix[i - 1][j] == 0) {
                             matrix[i - 1][j] = flag + 1;
-                        if (j != (matrix[i].length - 1) && matrix[i][j + 1] == 0)
+                            amt++;
+                        }
+                        if (j != (matrix[i].length - 1) && matrix[i][j + 1] == 0) {
                             matrix[i][j + 1] = flag + 1;
-                        if (j != 0 && matrix[i][j - 1] == 0)
+                            amt++;
+                        }
+                        if (j != 0 && matrix[i][j - 1] == 0) {
                             matrix[i][j - 1] = flag + 1;
+                            amt++;
+                        }
                     }
 
                 }
+
             }
 
-
-            day++;
+            if (amt > 0) day++;
             flag++;
             complete = true;
             for (int z = 0; z < matrix.length; z++) {
                 for (int c = 0; c < matrix[z].length; c++) {
                     if (matrix[z][c] == 0) {
                         complete = false;
+                        amt = 0;
                     }
                 }
             }
@@ -55,3 +65,6 @@ public class Level1 {
 
 
 }
+
+
+
